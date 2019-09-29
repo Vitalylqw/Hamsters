68c8a7a139e9707979e74bba0acc13e609693707
Vitalylqw <vitalylqw@gmail.com>
2019-09-29 21:18:27 +0300

    added hamsters.get_possition

--- hamsters.py
+++ hamsters.py
@@ -2,11 +2,19 @@ from random import randint
 
 
 class Hamster:
-
+    li_positions = [[0, 0]]
     def __init__(self,hid, x_n, y_n):
         self.hid=hid
         self.helth = 1 #randint(1, 4)
-        self.position = [randint(0, x_n-1), randint(0, y_n-1)]
+        self.position = self.get_possition(x_n,y_n)
+
+    def get_possition(self,x,y):
+        while True:
+            var_new_position=[randint(0, x - 1), randint(0, y - 1)]
+            if not var_new_position in self.li_positions:
+                self.li_positions.append(var_new_position)
+                return var_new_position
+
 
     def on_shot(self,hamsters):
         self.helth-=1
