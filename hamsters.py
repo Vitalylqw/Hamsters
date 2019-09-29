72d2cb9f5cd2cad4e712932b55a94791afbadd3b
Vitalylqw <vitalylqw@gmail.com>
2019-09-29 21:44:11 +0300

    added liefs to hamsters

--- hamsters.py
+++ hamsters.py
@@ -5,7 +5,7 @@ class Hamster:
     li_positions = [[0, 0]]
     def __init__(self,hid, x_n, y_n):
         self.hid=hid
-        self.helth = 1 #randint(1, 4)
+        self.helth = randint(1, 4)
         self.position = self.get_possition(x_n,y_n)
 
     def get_possition(self,x,y):
@@ -18,7 +18,12 @@ class Hamster:
 
     def on_shot(self,hamsters):
         self.helth-=1
-        print(self.hid+1, 'was kiled')
-        hamsters.remove(self)
+        if self.helth==0:
+            print(self.hid+1, 'was kiled')
+            hamsters.remove(self)
+            return True
+        else:
+            print(self.hid + 1, 'was not kiled')
+            return False
 
 
